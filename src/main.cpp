b6ade22b29e05d5343fdf14495ff312dc619d174
mash <matthew.ashorn@utexas.edu>
2024-11-28 20:21:22 -0600

    Added a real font we might use, created the window size correctly, centered the welcome text correctly.

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,25 @@
 int main()
 {
 	// create the window
-	sf::RenderWindow window(sf::VideoMode(600, 400), "Vehicle Maintenance Log");
+	sf::Vector2f window_resolution;
+	window_resolution.x = 1280;
+	window_resolution.y = 720;
+
+	sf::RenderWindow window(sf::VideoMode(window_resolution.x, window_resolution.y), "Vehicle Maintenance Log");
 
 	// Prepare the text
 	sf::Text welcomeText;
-	welcomeText.setCharacterSize(30);
-	welcomeText.setFillColor(sf::Color::Cyan);
+	welcomeText.setCharacterSize(60);
+	welcomeText.setFillColor(sf::Color::White);
+	welcomeText.setScale(0.5, 0.5);
 	sf::Font font;
-	font.loadFromFile("fonts/DS-DIGI.TTF");
+	font.loadFromFile("fonts/Iceland-Regular.ttf");
 	welcomeText.setFont(font);
-	welcomeText.setPosition(0, 100); // update to upper center of window size
 	welcomeText.setString("Welcome to the Vehicle Maintenance Log");
+	welcomeText.setPosition((window_resolution.x / 2) - (welcomeText.getGlobalBounds().width/2), 0); 
 
 	// Prepare the View
-	sf::View mainView(sf::FloatRect(0, 0, 600, 400)); // update to window size dynamically
+	sf::View mainView(sf::FloatRect(0, 0, window_resolution.x, window_resolution.y)); 
 	window.setView(mainView);
 
 	while (window.isOpen())
